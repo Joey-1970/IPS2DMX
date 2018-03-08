87550f47b74fc827bf0ff57bb820c9580f2b7806
Joey-1970 <paeper@horburg.de>
2018-03-08 18:22:38 +0100

    Update module.php

--- IPS2DMX_ProgramTrigger/module.php
+++ IPS2DMX_ProgramTrigger/module.php
@@ -30,7 +30,7 @@
 		$arrayElements = array(); 
 		$arrayElements[] = array("name" => "Open", "type" => "CheckBox",  "caption" => "Aktiv"); 
  		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
-		
+		$arrayElements[] = array("type" => "IntervalBox", "name" => "Timer_1",  "caption" => "Timer (sek)"); 
 		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________"); 
 		$arrayActions = array();
 		$arrayActions[] = array("type" => "Label", "label" => "Diese Funktionen stehen erst nach Eingabe und Übernahme der erforderlichen Daten zur Verfügung!");
