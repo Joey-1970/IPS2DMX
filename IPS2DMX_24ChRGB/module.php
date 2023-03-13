c56c1ba2d974ff3cc40de82e1608c5fa21bb6750
Joey-1970 <paeper@horburg.de>
2023-03-13 17:59:58 +0100

    Update module.php

--- IPS2DMX_24ChRGB/module.php
+++ IPS2DMX_24ChRGB/module.php
@@ -223,9 +223,9 @@
 						$this->SendDebug("SetChannelValue", "Gruppe ".($i + 1)."gesendet", 0);
 						$this->SendDataToParent(json_encode(Array("DataID"=> "{F241DA6A-A8BD-484B-A4EA-CC2FA8D83031}", "Size" => 1,  "Channel" => $DMXChannel, "Value" => $Value, "FadingSeconds" => 0.0, "DelayedSeconds" => 0.0 )));
 					}
-					$this->SetValue("Intensity_R_".($i + 1), $Value_R);
-					$this->SetValue("Intensity_G_".($i + 1), $Value_G);
-					$this->SetValue("Intensity_B_".($i + 1), $Value_B);
+					$ChannelArray = ["R", "G", "B"];
+					$this->SetValue("Intensity_".$ChannelArray($Channel)."_".($i + 1), $Value);
+					
 					$this->SetValue("Color_RGB_".$Group, $this->RGB2Hex($Value_R, $Value_G, $Value_B));
 				}
 			}
