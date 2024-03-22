08d8fcf520ce5186a41a0ef88a85639eb00524cc
Joey-1970 <paeper@horburg.de>
2024-03-22 10:13:29 +0100

    Update module.php

--- IPS2DMX_UKingMovingHead/module.php
+++ IPS2DMX_UKingMovingHead/module.php
@@ -316,16 +316,16 @@
 		If ($this->ReadPropertyBoolean("Open") == true) {	
 			// Pan
 			//$this->SetValue("PanMotion", $Pan);
-			$this->SetChannelValue( 1, $Pan, $this->GetIDForIdent("PanMotion"));
+			$this->SetChannelValue( 1, $Pan, "PanMotion");
 			// Tilt
 			//$this->SetValue("TiltMotion", $Tilt);
-			$this->SetChannelValue( 3, $Tilt, $this->GetIDForIdent("TiltMotion"));
+			$this->SetChannelValue( 3, $Tilt, "TiltMotion");
 			// Color
 			//$this->SetValue("Color", $Color);
-			$this->SetChannelValue( 5, $Color, $this->GetIDForIdent("Color"));
+			$this->SetChannelValue( 5, $Color, "Color");
    			// Gobo
       			//$this->SetValue("Gobo", $Gobo);
-			$this->SetChannelValue( 6, $Gobo, $this->GetIDForIdent("Gobo"));
+			$this->SetChannelValue( 6, $Gobo, "Gobo");
 		}
 	}
 	
